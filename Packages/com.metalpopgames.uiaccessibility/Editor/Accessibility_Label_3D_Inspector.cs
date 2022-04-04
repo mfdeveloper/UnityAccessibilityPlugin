7427c6b17af8268e9fc0dce712d28ea95f4edf3a
Felipe Ferreira <mfelipeof@gmail.com>
2022-04-05 00:33:03 +0100

    [WIP]: UPM package structure

---
+++ Packages/com.metalpopgames.uiaccessibility/Editor/Accessibility_Label_3D_Inspector.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+namespace UAP
+{
+	[CustomEditor(typeof(AccessibleLabel_3D)), CanEditMultipleObjects]
+	public class Accessibility_Label_3D_Inspector : Accessibility_InspectorShared
+	{
+		//////////////////////////////////////////////////////////////////////////
+
+		void OnEnable()
+		{
+		}
+
+		//////////////////////////////////////////////////////////////////////////
+
+		public override void OnInspectorGUI()
+		{
+			SetupGUIStyles();
+			serializedObject.Update();
+
+			EditorGUILayout.Separator();
+			EditorGUILayout.LabelField("Label", myHeadingStyle);
+			EditorGUILayout.Separator();
+			
+			// Name
+			DrawNameSection(false);
+
+			// Positioning / Traversal
+			DrawPositionOrderSection(false);
+
+			// Speech Output
+			DrawSpeechOutputSection();
+
+			// Callbacks
+			DrawCallbackSection(false);
+
+
+			serializedObject.ApplyModifiedProperties();
+			DrawDefaultInspectorSection();
+		}
+
+	}
+}
---
+++ Packages/com.metalpopgames.uiaccessibility/Samples/Match3GameExample/Scripts/instructions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+using UAP.Core;
+
+public class instructions : MonoBehaviour
+{
+	//////////////////////////////////////////////////////////////////////////
+
+	public void OnBackButtonPressed()
+	{
+		Instantiate(Resources.Load("Main Menu"));
+		DestroyImmediate(gameObject);
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+}
